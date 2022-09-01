c0efb5600071ee7183176c97c324d2d608587a73
Diana Bayazitova <onlydi@mail.ru>
2022-09-01 17:10:15 +0500

    Changed regEx

--- src/PersonsComparator.java
+++ src/PersonsComparator.java
@@ -11,10 +11,9 @@ public class PersonsComparator implements Comparator<Person> {
     public int compare(Person o1, Person o2) {
         String[] s;
         String[] s2;
-        s = o1.getSurname().split("-");
-        s = o1.getSurname().split(" ");
-        s2 = o2.getSurname().split("-");
-        s2 = o2.getSurname().split(" ");
+
+        s = o1.getSurname().split("\\w");
+        s2 = o2.getSurname().split("\\w");
         if (s.length >= numberOfSurname && s2.length >= numberOfSurname) {
             return Integer.compare(o2.getAge(), o1.getAge());
         } else if (s.length < s2.length) {
