1167c43359fc09f77c77f27701b13eabea92ebfd
Diana Bayazitova <onlydi@mail.ru>
2022-09-03 19:31:50 +0500

    Added lambda expressions

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 public class Main {
+
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
         people.add(new Person("Anya", "Orlova", 25));
@@ -12,8 +16,29 @@ public class Main {
         people.add(new Person("Marat", "Petrov Ivanov Fpolom Trhfgd", 55));
         people.add(new Person("Konstantin", "Pet", 65));
         System.out.println(people);
-        Collections.sort(people, new PersonsComparator(2));
+
+        people.sort((o1,o2)->{
+            String[] s;
+            String[] s2;
+            int numberOfSurname=2;
+            s = o1.getSurname().split("\\w");
+            s2 = o2.getSurname().split("\\w");
+                if (s.length >= numberOfSurname && s2.length >= numberOfSurname) {
+                    return Integer.compare(o2.getAge(), o1.getAge());
+                }
+            if (s.length < s2.length) {
+                return 1;
+            } else if (s.length > s2.length) {
+                return -1;
+            } else {
+                return Integer.compare(o2.getAge(), o1.getAge());
+            }
+        });
         System.out.println(people);
     }
 
-}
+    }
+
+
+
+
